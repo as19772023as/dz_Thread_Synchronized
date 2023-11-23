a32a324859f9ffa9fd49eabab5b247bcc6d66f5c
Andrey Strebkov <as19772023as@gmail.com>
2023-11-23 18:25:16 +0300

    Option main-real

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,37 @@ public class Main {
     public static int countKey = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        String letters = generateRoute(letter, length);
+
         for (int i = 0; i < length; i++) {
             new Thread(() -> {
-                countFreq(letters);
+                String letters = generateRoute(letter, 100);
+                int frequency = (int) letters.chars().filter(ch->ch == 'R').count();
+
+                synchronized (sizeToFreq){
+                    if(sizeToFreq.containsKey(frequency)){
+                        sizeToFreq.put(frequency, sizeToFreq.get(frequency) + 1);
+                    } else {
+                        sizeToFreq.put(frequency, 1);
+                    }
+                }
             }).start();
         }
 
-        Thread.sleep(300);
-        maxCountFreq(sizeToFreq);
+        Map.Entry<Integer, Integer> max = sizeToFreq.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get();
+        System.out.println("Самое частое количество повторений " + max.getKey() +
+                " (встретилось " + max.getValue() + "  раз)");
+
+        System.out.println("Другие размеры: ");
+        sizeToFreq.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(e-> System.out.println(" - " + e.getKey()  + " встретилось " + e.getValue() + "  раз)"));
+
     }
+
 
     public static String generateRoute(String letters, int length) {
         Random random = new Random();
@@ -28,38 +49,33 @@ public class Main {
         return route.toString();
     }
 
-    public static Map<Integer, Integer> countFreq(String routes) {
-        for (int i = 0; i < routes.length(); i++) {
-            if (routes.charAt(i) == 'R') {
-                countKey++;
-            } else if (countKey > 0) {
-                synchronized (sizeToFreq) {
-                    sizeToFreq.put(countKey, sizeToFreq.getOrDefault(countKey, 0) + 1);
-                }
-                countKey = 0;
-            }
-        }
-        return sizeToFreq;
+//    public static Map<Integer, Integer> countFreq(String routes) {
+//        for (int i = 0; i < routes.length(); i++) {
+//            if (routes.charAt(i) == 'R') {
+//                countKey++;
+//            } else if (countKey > 0) {
+//                  synchronized (sizeToFreq) {
+//                sizeToFreq.put(countKey, sizeToFreq.getOrDefault(countKey, 0) + 1);
+//                 }
+//                countKey = 0;
+//            }
+//        }
+//        return sizeToFreq;
+//    }
+
+
+ //   public static void maxCountFreq(Map<Integer, Integer> map) {
+//        int max = 0;
+//        for (Integer key : map.keySet()) {
+//            Integer value = map.get(key);
+//            if(max < key) max = key;
+//        Map.Entry<Integer, Integer> maxFreq = map.entrySet()
+//                .stream()
+//                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+//                .findFirst()
+//                .get();
+//        System.out.println("Самое частое количество повторений " + maxFreq.getKey() +
+//                " (встретилось " + maxFreq.getValue() + "  раз)");
+//        }
     }
 
-
-    public static void maxCountFreq(Map<Integer, Integer> map) {
-        Map.Entry<Integer, Integer> maxFreq = map
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
-                .findFirst()
-                .get();
-        System.out.println("Самое частое количество повторений " + maxFreq.getKey() +
-                " (встретилось " + maxFreq.getValue() + "  раз)");
-
-        System.out.println("Другие размеры: ");
-
-        for (Map.Entry<Integer, Integer> maxFreq2 : map.entrySet()) {
-            if (maxFreq.getKey() == maxFreq2.getKey()) {
-                continue;
-            }
-            System.out.println("- " + maxFreq2.getKey() + " ( " + maxFreq2.getValue() + "  раз)");
-        }
-    }
-}
